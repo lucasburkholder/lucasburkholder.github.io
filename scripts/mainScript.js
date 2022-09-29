310f448cf44bc30a5675a6221cd7ab857052ca3f
lucasburkholder <guitarflake27@gmail.com>
2022-09-29 17:59:37 -0400

    Initial site upload

---
+++ scripts/mainScript.js
@@ -0,0 +1,37 @@
+function includeHTML() {
+    console.log("calling function");
+    var z, i, elmnt, file, xhttp;
+    z = document.getElementsByTagName("*");
+    for (i = 0; i < z.length; i++) {
+        elmnt = z[i];
+        file = elmnt.getAttribute("w3-include-html");
+        if (file) {
+            xhttp = new XMLHttpRequest();   
+            xhttp.onreadystatechange = function() {
+                if (this.readyState == 4) {
+                    if (this.status == 200) {
+                        elmnt.innerHTML = this.responseText;
+                    }
+                    if (this.status == 404) {
+                        elmnt.innerHTML = "Included HTML page not found";
+                    }
+                    // remove the attribute, call this function again
+                    elmnt.removeAttribute("w3-include-html");
+                    includeHTML();
+                }
+            }
+            xhttp.open("GET", file, true);
+            xhttp.send();
+            return;
+        }
+    }
+}
+
+function showMobileMenu() {
+    var x = document.getElementById("mobileMenu");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
